088c182ca90fd01ad94eeac2b7f35e8d9a665f05
vgerevini <vgerevini@gmail.com>
2017-03-19 22:24:53 -0300

    classe produto

---
+++ TVG.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVG.LojaVirtual.Dominio.Entidade
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preço { get; set; }
+        public string Categoria { get; set; }
+    }
+}
+
+
