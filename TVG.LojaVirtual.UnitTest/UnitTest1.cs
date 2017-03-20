12858e5dbe89124efc120b2bef875bacb983e38d
vgerevini <vgerevini@gmail.com>
2017-03-19 21:40:32 -0300

    Git

---
+++ TVG.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TVG.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestTVG
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
