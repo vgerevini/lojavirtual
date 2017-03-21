1da94fa8b7c5809ef3ef1c16b0310f1451ff5134
vgerevini <vgerevini@gmail.com>
2017-03-20 21:25:37 -0300

    Classes de Paginacao

--- TVG.LojaVirtual.UnitTest/UnitTest1.cs
+++ TVG.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TVG.LojaVirtual.UnitTest
@@ -7,8 +8,25 @@ namespace TVG.LojaVirtual.UnitTest
     public class UnitTestTVG
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            int[] numeros = {5,4,1,3,9,8,6,7,2,0};
+            var resultado = from num in numeros.Take(5) select num;
+
+            int[] teste = { 5, 4, 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
-    }
+
+        [TestMethod]
+        public void Skip()
+        {
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+            int[] teste = { 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+    }    
 }
---
+++ TVG.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using TVG.LojaVirtual.Web.Models;
+
+namespace TVG.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+
+        //Total de paginas = 3
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPaginas; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                tag.AddCssClass("btn btn-default");
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
